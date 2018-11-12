d2d06145fde09524f26361615d013b64e70550a1
Lucas Reis <reisaolucas@gmail.com>
2018-11-12 16:46:50 -0200

    x-totvs tab implemented

--- src/app/Entities/TASEntity.ts
+++ src/app/Entities/TASEntity.ts
@@ -5,7 +5,11 @@ export class TasEntity {
 }
 
 export class TasInfoEntity {
+    'description': string;
+    'version': string;
+    'title': string;
     'contact': TasInfoContactEntity = new TasInfoContactEntity();
+    'x-totvs': TasInfoXTotvsEntity = new TasInfoXTotvsEntity();
     'license': TasInfoLicenseEntity = new TasInfoLicenseEntity();
 }
 
@@ -17,6 +21,25 @@ export class TasInfoContactEntity {
 export class TasInfoLicenseEntity {
     'name': string;
     'url': string;
+}
+
+export class TasInfoXTotvsEntity {
+    'messageDocumentation': TasInfoXTotvsMesDocEntity = new TasInfoXTotvsMesDocEntity();
+    'productInformation': Array<TasInfoXTotvsProdInfoEntity> = new Array<TasInfoXTotvsProdInfoEntity>();
+}
+
+export class TasInfoXTotvsMesDocEntity {
+    'description': string;
+    'name': string;
+    'segment': string;
+}
+
+export class TasInfoXTotvsProdInfoEntity {
+    'product': string;
+    'adapter': string;
+    'contact': string;
+    'description': string;
+    'helpUrl': string;
 }
 
 export class TasServerEntity {
--- src/app/main-frame/xtotvs-tab/xtotvs-tab.component.ts
+++ src/app/main-frame/xtotvs-tab/xtotvs-tab.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TasInfoEntity } from 'src/app/Entities/TASEntity';
 
 @Component({
   selector: 'app-xtotvs-tab',
   templateUrl: './xtotvs-tab.component.html'
 })
 export class XtotvsTabComponent implements OnInit {
+  @Input() infoProperty: TasInfoEntity;
 
   constructor() { }
-
   ngOnInit() {
   }
 
